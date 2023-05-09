e179aadc288369a5c70953a8288b10f0baa1abdf
andyneo123 <andyneo123@gmail.com>
2023-05-09 22:14:57 +0800

    Add index.js of multiple pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Essay from './pages/Essay';
+import Scholarship from './pages/Scholarship';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={'/'} element={<Home />} />
+      <Route path={'/about'} element={<About />} />
+      <Route path={'/contact'} element={<Contact />} />
+      <Route path={'/essay'} element={<Essay />} />
+      <Route path={'/scholarship'} element={<Scholarship />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Essay/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Essay = () => {
+  return (
+    <div>essay</div>
+  );
+};
+
+export default Essay;
