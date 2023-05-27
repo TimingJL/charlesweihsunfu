1953219e60be4b229905edb2672f848df61affd6
just_taiming <eefozeo@gmail.com>
2023-05-27 18:11:19 +0800

    Styling font

--- src/pages/Home/Introduction.jsx
+++ src/pages/Home/Introduction.jsx
@@ -7,12 +7,32 @@ import Button from '@mui/material/Button';
 import CardImg1 from '../../assets/hsilai.jpg';
 import SDSU_Img from '../../assets/SDSU_Arts_&_Letters_New_Building.jpg';
 
+const StyledContainer = styled(Container)`
+  margin-top: 100px;
+  margin-bottom: 100px;
+  @media
+  ${(props) => props.theme.device.smTablet},
+  ${(props) => props.theme.device.mobile},
+  {
+    margin-top: 40px;
+    margin-bottom: 40px;
+  }
+`;
+
 const Image = styled.div`
   background: url(${props => props.$src});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   border-radius: 8px;
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 36px;
+  margin-bottom: 12px;
+  font-family: 'Playfair Display';
+  color: rgb(7, 24, 61);
+  text-transform: uppercase;
 `;
 
 const Card = styled.div`
@@ -36,8 +56,10 @@ const Card = styled.div`
 const Content = styled.div``;
 
 const Title = styled.div`
-    color: black;
+    color: rgb(7, 24, 61);
     font-size: 24px;
+    font-family: 'Playfair Display';
+    font-weight: 700;
 
     ${(props) => props.theme.device.smTablet},
     ${(props) => props.theme.device.mobile},
@@ -56,30 +78,40 @@ const buttonSX = { width: '100px', marginTop: 2, boxShadow: 'none' };
 
 const Introduction = () => {
   return(
-    <Container maxWidth="md" sx={{ marginTop: '100px', marginBottom: '100px' }}>
-      <Stack spacing={4}>
-        <Card>
-          <Image $src={CardImg1} />
-          <Content>
-            <Title>An Introduction to the Charles Wei-Hsun Fu Foundation</Title>
-            <Description>The Charles Wei-Hsun FU FOUNDATION, a tax-exempt foundation, was organized in 1997 in loving memory of the life and work of CHARLES WEI-HSUN FU (1933-1996),. . . .</Description>
-            <Stack spacing={4} alignItems="flex-end">
-              <Button variant="contained" sx={buttonSX}>More</Button>
-            </Stack>
-          </Content>
-        </Card>
-        <Card>
-          <Image $src={SDSU_Img} />
-          <Content>
-            <Title>The Charles Wei-Hsun Fu Scholarship in Chinese Thought & Culture</Title>
-            <Description>beginning in Fall, 2005 in cooperation with the SDSU College of Arts & Letters and the China Studies Institute, . . .</Description>
-            <Stack spacing={4} alignItems="flex-end">
-              <Button variant="contained" sx={buttonSX}>More</Button>
-            </Stack>
-          </Content>
-        </Card>
+    <StyledContainer maxWidth="md">
+      <Stack spacing={8}>
+        <div>
+          <Stack alignItems="center">
+            <SectionTitle>Introduction</SectionTitle>
+          </Stack>
+          <Card>
+            <Image $src={CardImg1} />
+            <Content>
+              <Title>An Introduction to the Charles Wei-Hsun Fu Foundation</Title>
+              <Description>The Charles Wei-Hsun FU FOUNDATION, a tax-exempt foundation, was organized in 1997 in loving memory of the life and work of CHARLES WEI-HSUN FU (1933-1996),. . . .</Description>
+              <Stack spacing={4} alignItems="flex-end">
+                <Button variant="contained" sx={buttonSX}>More</Button>
+              </Stack>
+            </Content>
+          </Card>
+        </div>
+        <div>
+          <Stack alignItems="center">
+            <SectionTitle>Scholarship</SectionTitle>
+          </Stack>
+          <Card>
+            <Image $src={SDSU_Img} />
+            <Content>
+              <Title>The Charles Wei-Hsun Fu Scholarship in Chinese Thought & Culture</Title>
+              <Description>beginning in Fall, 2005 in cooperation with the SDSU College of Arts & Letters and the China Studies Institute, . . .</Description>
+              <Stack spacing={4} alignItems="flex-end">
+                <Button variant="contained" sx={buttonSX}>More</Button>
+              </Stack>
+            </Content>
+          </Card>
+        </div>
       </Stack>
-    </Container>
+    </StyledContainer>
   );
 };
 
